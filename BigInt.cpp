5d66aadf751e6ffb6e705bde8f647bf8ab5a2aec
Sergei <w.silton.it@gmail.com>
2024-05-10 20:05:32 +0300

    Create class BigInt

---
+++ BigInt.cpp
@@ -0,0 +1,123 @@
+#include "BigInt.h"
+
+std::ostream& operator<<(std::ostream& os, const BigInt& num) {
+	size_t start_num = 0;
+	for(size_t i = 0; i < num._amount_digits; ++i)
+		if(num._digits[i] != 0)
+			start_num = i;
+
+	for(int i = start_num; i >= 0; --i)
+		os << num._digits[i];
+
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, BigInt& num) {
+	char* new_num = new char[num._amount_digits];
+	is >> new_num;
+	
+	size_t num_size = 0;
+	while(new_num[num_size] != 0)
+		++num_size;
+	
+	for(size_t i = 0; i < num._amount_digits; ++i)
+		num._digits[i] = 0;
+
+	for(size_t i = 0; i < num_size - 1; ++i) 
+		num._digits[num_size - i - 1] += new_num[i] - '0';
+	
+	delete[] new_num;
+	return is;
+}
+
+BigInt operator+(BigInt res, const BigInt& num) {
+	res += num;
+	return res;
+}
+
+BigInt operator*(const BigInt& a, const BigInt& b) {
+	BigInt res;
+	
+	for(size_t i = 0; i < a._amount_digits; ++i)
+		for(size_t j = 0; j < a._amount_digits - i; ++j)
+			res._digits[i + j] += a._digits[i] * b._digits[j];
+	
+	for(size_t i = 0; i < res._amount_digits - 1; ++i) {
+		res._digits[i + 1] += res._digits[i] / res._BASE;
+		res._digits[i] %= res._BASE;
+	}
+
+	return res;
+}
+
+BigInt::BigInt(): _amount_digits(100), _digits(new int[_amount_digits]) {
+	for(size_t i = 0; i < _amount_digits; ++i)
+		_digits[i] = 0;
+}
+
+BigInt::BigInt(size_t num): BigInt() {
+	size_t next = 0;
+	while(num) {
+		_digits[next++] = num % _BASE;
+		num /= _BASE;
+	}
+}
+
+BigInt::BigInt(const BigInt& other): _amount_digits(other._amount_digits), _digits(new int[_amount_digits]) {
+	for(size_t i = 0; i < _amount_digits; ++i)
+		_digits[i] = other._digits[i];
+}
+
+BigInt& BigInt::operator=(BigInt other) {
+	swap(other);
+	return *this;
+}
+
+BigInt::BigInt(BigInt&& other) noexcept : _amount_digits(other._amount_digits), _digits(other._digits) {
+	other._digits = nullptr;
+}
+
+BigInt& BigInt::operator=(BigInt&& other) noexcept {
+	BigInt res(std::move(other));
+	swap(res);
+
+	return *this;
+}
+
+void BigInt::swap(BigInt& other) noexcept {
+	std::swap(_amount_digits, other._amount_digits);
+	std::swap(_digits, other._digits);
+}
+
+BigInt::~BigInt() {
+	delete[] _digits;
+}
+
+void BigInt::operator+=(const BigInt& other) {
+	for(size_t i = 0; i < _amount_digits; ++i)
+		_digits[i] += other._digits[i];
+
+	for(size_t i = 0; i < _amount_digits - 1; ++i) {
+		_digits[i + 1] += _digits[i] / _BASE;
+		_digits[i] %= _BASE;
+	}
+		
+}
+
+void BigInt::operator*=(const BigInt& other) {
+	BigInt res = std::move(*this * other);
+	*this = res;
+}
+
+int BigInt::operator[](const size_t& ind) {
+	if(ind < 0 || ind >= _amount_digits)
+		abort();
+
+	return _digits[ind];
+}
+
+void BigInt::set_amount_digits(size_t amount) {
+	_amount_digits = amount;
+	delete[] _digits;
+	_digits = new int[_amount_digits];
+}
---
+++ BigInt.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <utility>
+
+#define size_t unsigned int 
+
+class BigInt {
+	friend std::ostream& operator<<(std::ostream&, const BigInt&);
+
+	friend std::istream& operator>>(std::istream&, BigInt&);
+
+	friend BigInt operator+(BigInt, const BigInt&);
+
+	friend BigInt operator*(const BigInt&, const BigInt&);
+
+public:
+	BigInt();
+
+	BigInt(size_t);
+
+	BigInt(const BigInt&);
+
+	BigInt& operator=(BigInt);
+
+	BigInt(BigInt&&) noexcept;
+
+	BigInt& operator=(BigInt&&) noexcept;
+
+	void swap(BigInt&) noexcept;
+
+	~BigInt();
+
+	void operator+=(const BigInt&);
+
+	void operator*=(const BigInt&); 
+
+	int operator[](const size_t&);
+
+	void set_amount_digits(size_t);
+
+private:
+	//для перехода в сторичную сиситему изменить на 100
+	//стоит 10 так как ндао вычислисть 100 факториал в десятичной
+	static const size_t _BASE = 100;
+	size_t _amount_digits;
+	
+	int* _digits;
+};
